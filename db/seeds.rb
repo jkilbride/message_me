dd5910cbf9370aa0c4686b501b7c20dc79f20b46
John Kilbride <jkilbride@gmail.com>
2020-03-12 09:16:17 +0000

    messages migrations and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "jkilbride", password:"password")
+User.create(username: "akilbride", password:"password")
+User.create(username: "pkirwan", password:"password")
+User.create(username: "sbyrne", password:"password")
+User.create(username: "abyrne", password:"password")
